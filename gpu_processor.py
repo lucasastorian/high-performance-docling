8e3ba23d9944ac319bc44fdb46834457e2a9639e
Lucas Astorian <lucas@leadreel.nyc>
2025-08-12 00:31:29 -0400

    bug fix

--- gpu_processor.py
+++ gpu_processor.py
@@ -13,7 +13,7 @@ from docling_core.types.doc import DocItemLabel
 from docling_core.types.doc.page import TextCell, BoundingRectangle
 import numpy as np
 
-from optimized.layout_model import LayoutModel
+from optimized.layout.layout_model import LayoutModel
 from standard.table_structure_model import TableStructureModel
 from optimized.table.table_timing_debug import print_timing_summary
 from table_regression_runner import TableRegressionRunner, Tolerances
